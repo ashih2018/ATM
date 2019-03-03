fad13ef71be9f37d3b33e4f7249f60fd70f028e7
Samarth Patel <samarth.patel.22@hotmail.com>
2019-03-03 15:53:03 -0500

    Added basic attributes to Account.java and Transaction.java, used BigDecimal data type for representing currency

--- phase1/ATM_0354/Account.java
+++ phase1/ATM_0354/Account.java
@@ -1,4 +1,22 @@
 package ATM_0354;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 public abstract class Account {
+    private ArrayList<Transaction> transactions;
+
+    private int id;
+    private BigDecimal balance;
+
+    public Account() {
+        this.transactions = new ArrayList<>();
+        this.balance = new BigDecimal(0);
+        // TODO: Set id and figure out a method to create unique ID's for new accounts
+    }
+
+    public String summary() {
+        return "Account ID: " + this.id + "\nAccount Balance: " + this.balance;
+    }
+
 }
--- phase1/ATM_0354/Transaction.java
+++ phase1/ATM_0354/Transaction.java
@@ -1,4 +1,18 @@
 package ATM_0354;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Transaction {
+    private Date date;
+    private int fromId, toId;
+    private BigDecimal value;
+
+    public Transaction(int fromId, int toId, BigDecimal value) {
+        this.date = new Date();
+        this.fromId = fromId;
+        this.toId = toId;
+        this.value = value;
+    }
+
 }
