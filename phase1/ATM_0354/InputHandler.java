39b0da9801cce5faac16e6240f4c8f6b71810854
Sky Li <skylimail42@gmail.com>
2019-03-04 16:10:20 -0500

    started working on inputhandler

--- phase1/ATM_0354/InputHandler.java
+++ phase1/ATM_0354/InputHandler.java
@@ -1,4 +1,41 @@
 package ATM_0354;
 
+import java.util.*;
+
 public class InputHandler {
+    private Map<String, InputMethod> inputMap;
+    /*
+    Possible Actions:
+        First time setting up program:
+            Set up Bank Manager
+            Set up time <-- done automatically
+            Set up initial cash in ATM <-- done automatically
+        On Day start
+            Login <-- done
+        BankManager options:
+            Set up a new User (username, password, account(s), initial deposit?)
+            Check for account creation requests
+                Approve
+                Decline
+            Refill cash
+            Undo most recent transaction
+            Logout
+        User Actions
+           Request new account
+           View Accounts
+                Withdraw moneyTransfer money
+                Deposit money
+           Deposit money into a checking account (default)
+            Logout
+
+     */
+    public static void setupInputs(){
+
+    }
+
+    public static void handleInput(String input){
+
+    }
+
 }
+
---
+++ phase1/ATM_0354/inputMethods/createNewUserMethod.java
@@ -0,0 +1,11 @@
+package ATM_0354.inputMethods;
+
+import ATM_0354.InputMethod;
+
+import java.util.Scanner;
+
+public class createNewUserMethod implements InputMethod {
+    public boolean run(Scanner in){
+        return false;
+    }
+}
