8c0241e249ef9167271836298f2bac635321b057
Stella Cai <stella.sijiacai@gmail.com>
2019-03-04 15:56:48 -0500

    implemented CashObject class

--- phase1/ATM_0354/CashObject.java
+++ phase1/ATM_0354/CashObject.java
@@ -1,4 +1,20 @@
 package ATM_0354;
 
 public class CashObject {
+
+    private int cashValue;
+    private int count;
+
+    public CashObject(int cashValue, int count){
+        this.cashValue = cashValue;
+        this.count = count;
+    }
+
+    public int getCashValue() {
+        return cashValue;
+    }
+
+    public void setCashValue(int cashValue) {
+        this.cashValue = cashValue;
+    }
 }
