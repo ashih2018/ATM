aa725e9895407d21090e746b7e807710b9b13bd5
Stella Cai <stella.sijiacai@gmail.com>
2019-03-04 16:49:50 -0500

    Started working on CashHandler using Observer design pattern

--- phase1/ATM_0354/CashHandler.java
+++ phase1/ATM_0354/CashHandler.java
@@ -1,4 +1,15 @@
 package ATM_0354;
 
-public class CashHandler {
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+public class CashHandler implements Observer {
+
+    private ArrayList<CashObject> cash = new ArrayList<>();
+
+    @Override
+    public void update(Observable o, Object arg) {
+        
+    }
 }
