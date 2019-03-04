10105e419406775af0763af20a4deb5c25bf36e8
Sky Li <skylimail42@gmail.com>
2019-03-04 00:38:13 -0500

    Filled in methods / attributes for UserHandler / ATM / Main

--- phase1/ATM_0354/Main.java
+++ phase1/ATM_0354/Main.java
@@ -1,9 +1,19 @@
 package ATM_0354;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 public class Main {
     static ATM atm;
-    public static void main(String[] args){
-        atm = new ATM(new Date());
+
+    public static void main(String[] args) {
+        atm = new ATM();
+        atm.setDateTime(LocalDateTime.now());
+        Scanner in = new Scanner(System.in); //Set up for console input right now
+
+        //example nput ftomm console
+        System.out.println("What is your name?");
+        String name = in.nextLine();
+        System.out.println("Your name is: " + name);
     }
 }
