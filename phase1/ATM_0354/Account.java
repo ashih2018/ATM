97b4498986dd955fb014a355aabbd14f7081844d
Samarth Patel <samarth.patel.22@hotmail.com>
2019-03-04 18:02:41 -0500

    Added some getters for Account

--- phase1/ATM_0354/Account.java
+++ phase1/ATM_0354/Account.java
@@ -1,6 +1,7 @@
 package ATM_0354;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public abstract class Account {
@@ -8,11 +9,25 @@ public abstract class Account {
 
     private int id;
     private BigDecimal balance;
+    private LocalDateTime dateOfCreation;
 
     public Account(int id) {
         this.transactions = new ArrayList<>();
         this.balance = new BigDecimal(0);
         this.id = id;
+        this.dateOfCreation = LocalDateTime.now();
+    }
+
+    public Transaction getLastTransaction() {
+        return this.transactions.get(this.transactions.size() - 1);
+    }
+
+    public LocalDateTime getDateOfCreation() {
+        return this.dateOfCreation;
+    }
+
+    public BigDecimal getBalance() {
+        return this.balance;
     }
 
     @Override
--- phase1/ATM_0354/ChequingAccount.java
+++ phase1/ATM_0354/ChequingAccount.java
@@ -2,8 +2,18 @@ package ATM_0354;
 
 public class ChequingAccount extends AssetAccount {
 
+    private boolean primary;
+
     public ChequingAccount(int id) {
         super(id);
+        this.primary = false;
     }
 
+    public void setPrimary(boolean primary) {
+        this.primary = primary;
+    }
+
+    public boolean isPrimary() {
+        return primary;
+    }
 }
