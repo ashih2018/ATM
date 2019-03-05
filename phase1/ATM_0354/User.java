b5304454e843b08e813fce30e7dfe22d5c18e361
Samarth Patel <samarth.patel.22@hotmail.com>
2019-03-05 15:43:23 -0500

    Refactored some User methods

--- phase1/ATM_0354/User.java
+++ phase1/ATM_0354/User.java
@@ -3,6 +3,7 @@ package ATM_0354;
 import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -12,12 +13,12 @@ public class User extends Person {
     private ArrayList<Account> accounts;
     private Date creationDate;
     private DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-    private int accountID;
+    //private int accountID;
 
     public User(String username, String password) {
         super(username, password);
         accounts = new ArrayList<>();
-        accountID = 0;
+        //accountID = 0;
         creationDate = new Date();
     }
 
@@ -25,59 +26,60 @@ public class User extends Person {
         return dateFormat.format(creationDate);
     }
 
-    public int getAccountID() {
-        return accountID;
-    }
+//    public int getAccountID() {
+//        return accountID;
+//    }
 
-    public Account getAccount(int accountNum) {
-        try {
-            return accounts.get(accountNum);
-        }
-        catch (IndexOutOfBoundsException e) {
-            return null;
-        }
+    public Account getAccount(int accountId) {
+        for (Account account : this.accounts) {
+            if (account.getId() == accountId) return account;
+        } return null;
     }
 
     public void addAccount(Account account) {
-        //TODO: complete
+        this.accounts.add(account);
     }
 
     public String getSummary() {
-        String summary = "";
+        // Use a StringBuilder instead of String when we are mutating a string is better practice
+        StringBuilder summary = new StringBuilder();
         for (Account account: this.accounts) {
-            summary += account.toString();
-            summary += "\n";
+            summary.append(account.toString());
+            summary.append("\n");
         }
-        return summary;
+        return summary.toString();
     }
 
-    public String getLastTransaction(int accountNum) {
-        Account account;
-        try {
-            account = this.accounts.get(accountNum);
+    public Transaction getLastTransaction(int accountId) {
+        Account account = this.getAccount(accountId);
+        if (account == null) {
+            System.out.println("Account with id " + accountId + " doesn't exist ");
+            return null;
+        } else {
+            Transaction transaction = account.getLastTransaction();
+            if (transaction == null) {
+                System.out.println("User could not get last transaction from account");
+                return null;
+            } return transaction;
         }
-        catch (IndexOutOfBoundsException e) {
-            return("This account does not exist");
-        }
-
-        return(account.getLastTransaction().toString());
     }
 
-    public String getAccountDate(int accountNum) {
-        Account account;
-        try {
-            account = this.accounts.get(accountNum);
-        }
-        catch (IndexOutOfBoundsException e) {
-            return("This account does not exist");
-        }
-        return account.getDateOfCreation().toString();
+    public LocalDateTime getAccountDate(int accountId) {
+        Account account = this.getAccount(accountId);
+        if (account == null) {
+            System.out.println("Account does not exist");
+            return null;
+        } return account.getDateOfCreation();
     }
 
     public BigDecimal getAccountTotal() {
         BigDecimal total = new BigDecimal(0);
         for (Account account: this.accounts) {
-            total.add(account.getBalance());
+            if (account instanceof AssetAccount) {
+                total = total.add(account.getBalance().setScale(2, BigDecimal.ROUND_HALF_UP));
+            } else if (account instanceof DebtAccount) {
+                total = total.subtract(account.getBalance().setScale(2, BigDecimal.ROUND_HALF_UP));
+            }
         }
         return total;
     }
