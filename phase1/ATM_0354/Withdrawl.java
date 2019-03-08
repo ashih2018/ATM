d471a34608fb020ab0963209252615b182295b5f
Stella Cai <stella.sijiacai@gmail.com>
2019-03-08 15:20:52 -0500

    Fixed typo in Withdrawal

--- phase1/ATM_0354/Withdrawl.java
+++
@@ -1,9 +0,0 @@
-package ATM_0354;
-
-import java.math.BigDecimal;
-
-public class Withdrawl extends Transaction{
-    public Withdrawl (int accountIdFrom, int accountIdTo, BigDecimal value) {
-        super(accountIdFrom, accountIdTo, value, false);
-    }
-}
